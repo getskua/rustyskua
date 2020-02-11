ae7aeefccfff8a9b40ddbf20e8a928cdcd42cd60
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-11 13:26:04 +0000

    Finished frontmatter parser.

--- src/markdown.rs
+++ src/markdown.rs
@@ -19,8 +19,15 @@ impl Loc {
     }
 }
 
+pub struct Frontmatter {
+    values: HashMap<String, String>;
+}
+
 impl Frontmatter {
     pub fn new(string: &mut String) -> Self {
+        let mut frontmatter = Frontmatter {
+            values: HashMap::new()
+        };
         let mut loc = Loc::new(1, 0);
         let mut finished = false;
         while !finished {
@@ -30,11 +37,50 @@ impl Frontmatter {
                     let n_1 = string.pop().unwrap();
                     let n_2 = string.pop().unwrap();
                     if (n_1 != '-') | (n_2 != '-') {
-                        assert!("")
+                        assert!("The '-' on line {}, column {} is invalid.", loc.line, loc.col);
                     }
                 }
-            }
-        }
+                'a'..='z' | 'A'..='Z' => {
+                    let mut colon = false;
+                    let mut key = String::new();
+                    let mut value = String::new();
+                    while !colon {
+                        let n_char = string.pop().unwrap();
+                        match n_char {
+                            'a'..='z' | 'A'..='Z' | '_' => {
+                                key.push(n_char);
+                            }
+                            ':' => {
+                                colon = true;
+                            }
+                            _ => {
+                                panic!("Invalid token on line {}, column {}.", loc.line, loc.token);
+                            }
+                        }
+                    }
+                    let mut eol = false;
+                    while !eol {
+                        let n_char = string.pop().unwrap();
+                        match n_char {
+                            'a'..='z' | 'A'..='Z' | '_' => {
+                                key.push(n_char);
+                            }
+                            '\n' => {
+                                eol = true;
+                            }
+                            _ => {
+                                panic!("Invalid token on line {}, column {}.", loc.line, loc.token);
+                            }
+                        }
+                    }
+                    frontmatter.values.insert(key, value);
+                }
+                _ => {
+                    panic!("Unexpected token on line {}, column {}.", loc.line, loc.col)
+                }
+            };
+        };
+        frontmatter
     }
 }
 
